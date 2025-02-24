f212187b62185f6ae4126bec0cfcd95addc67f14
allexkap <aekpstn@gmail.com>
2025-02-24 22:49:10 +0300

    refactoring

--- leak_parser.py
+++ leak_parser.py
@@ -1,43 +1,36 @@
 import csv
 import random
-import sqlite3
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Any
 
 
 @dataclass
 class Table:
-    data: list
-    meta: tuple
+    name: str  # имя таблицы
+    data: list[list[Any]]  # сами данные
+    meta: list[str]  # имена полей
 
+    def get_sample(self, n: int = 10):
+        rows_count = len(self.data)
+        sample = [
+            self.data[i]
+            for i in random.sample(range(0, rows_count), min(rows_count, n))
+        ]
+        return Table(self.name, sample, self.meta)
 
-def parse_csv(path: Path) -> list[Table]:
-    with open(path) as file:
-        first_line = file.readline()
-        delim = max((first_line.count(d), d) for d in ",;\t")[1]
-        file.seek(0)
-        reader = csv.reader(file, delimiter=delim)
-        meta = tuple(next(reader))
-        content = [[*map(str.strip, lines)] for lines in reader]
+    def filtred(self, indexes: list[int]):
+        for row in self.data:
+            yield [row[i] for i in indexes]
 
-    return [Table(content, meta)]
-
-
-def get_sample(table: Table, n: int = 10):
-    rows_count = len(table.data)
-    content = [
-        table.data[i] for i in random.sample(range(0, rows_count), min(rows_count, n))
-    ]
-    return Table(content, table.meta)
-
-
-def reorder_columns(table: Table, indexes):
-    content = [[row[i] if i != -1 else "" for i in indexes] for row in table.data]
-    return Table(content, table.meta)
-
-
-def save_to_db(path: Path, table: Table):
-    print(*table.data, sep="\n")
+    def __add__(self, rhs: "Table"):
+        if (
+            self.name != rhs.name
+            or len(self.data) != len(rhs.data)
+            or self.meta != rhs.meta
+        ):
+            raise ValueError("nelzya")
+        return Table(self.name, self.data + rhs.data, self.meta)
 
 
 def ai_mock(table: Table, needed_columns: tuple) -> list[int]:
@@ -46,12 +39,33 @@ def ai_mock(table: Table, needed_columns: tuple) -> list[int]:
     ]
 
 
-def ai(table: Table, needed_columns: tuple) -> list[int]:
-    return ai_mock(table, needed_columns)
+@dataclass
+class AI:
+    def request(self, table: Table, needed_columns: tuple) -> list[int]:
+        return ai_mock(table, needed_columns)
 
 
-table = parse_csv(Path("./res/partselect.ru.csv"))[0]
-part = get_sample(table)
-indexe = ai(part, needed_columns=("LastName", "Email", "Phones"))
-out_table = reorder_columns(table, indexe)
-save_to_db(Path("out.db"), out_table)
+@dataclass
+class Params:
+    ai: AI  # интерфейс к ИИ, если парсеру требуется для внутренней работы
+    # и другое если кому то что то понадобится, расширяемость как никак
+
+
+def parse_csv(path: Path, params: Params) -> list[Table]:
+    with open(path) as file:
+        first_line = file.readline()
+        delim = max((first_line.count(d), d) for d in ",;\t")[1]
+        file.seek(0)
+        reader = csv.reader(file, delimiter=delim)
+        meta = list(next(reader))
+        content = [[*map(str.strip, lines)] for lines in reader]
+    return [Table(path.name, content, meta)]
+
+
+params = Params(AI())
+table = parse_csv(Path("./res/partselect.ru.csv"), params)[0]
+indexes = params.ai.request(
+    table.get_sample(), needed_columns=("LastName", "Email", "Phones")
+)
+for row in table.filtred(indexes):
+    print(row)
